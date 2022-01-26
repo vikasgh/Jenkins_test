66c575c819af11365fc4e8b17cd2e4acd8f519d3
Vikas Gupta <vikasgh@gmail.com>
2022-01-27 00:12:02 +0530

    V1.1

--- Jenkins_test/jentest.py
+++ Jenkins_test/jentest.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 def main():
   print("Date is "+ str(datetime.now().strftime("%d %B %Y %I:%M:%S %p")))
+  print("Version Change")
 if __name__ == "__main__":
   main()
